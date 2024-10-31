db5788d84270fcaae74bf0f1ed97abdb94f7911b
KadriGit <kadri.viira@tktk.ee>
2024-10-31 21:41:08 +0200

    final code

--- Week6Strings/Program.cs
+++ Week6Strings/Program.cs
@@ -5,15 +5,7 @@ Console.WriteLine(userName.Length);
 
 Console.WriteLine($"The first letter in your name is {userName[userName.Length -1]}");
 
-//a-counter 
-
-int counter = 0;
-for(int i = 0; i < userName.Length; i++)
-{
-    if (userName[i] == 'a')
-    {
-        counter++; // counter = counter + 1
-    }
-
-}
-Console.WriteLine($"Yor first name has {counter} a-letters");
+//replace
+// a - 4
+string UpdateUserName = userName.Replace("a", "4").Replace("i","3");
+Console.WriteLine(UpdateUserName);
