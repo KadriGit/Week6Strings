bd4c8dfcc80afc172451e7d0b27297f30eb946e2
KadriGit <kadri.viira@tktk.ee>
2024-10-31 21:38:14 +0200

    fifth

--- Week6Strings/Program.cs
+++ Week6Strings/Program.cs
@@ -5,3 +5,15 @@ Console.WriteLine(userName.Length);
 
 Console.WriteLine($"The first letter in your name is {userName[userName.Length -1]}");
 
+//a-counter 
+
+int counter = 0;
+for(int i = 0; i < userName.Length; i++)
+{
+    if (userName[i] == 'a')
+    {
+        counter++; // counter = counter + 1
+    }
+
+}
+Console.WriteLine($"Yor first name has {counter} a-letters");
